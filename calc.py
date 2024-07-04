20dd1029feaf7372d1de9663b0d5270247d93da8
Leeo <91311423+lethmeshine@users.noreply.github.com>
2024-07-04 16:04:24 +0200

    maj calc.py

--- calc.py
+++ calc.py
@@ -24,24 +24,11 @@ def conv(value):
         except ValueError:
             return str(value)
 
-def add2(arg1, arg2):
-    '''
-    The 'add2' function itself. It takes two arguments, converts them to their appropriate types
-    using the 'conv' function, and adds them together. If either argument is a string, it ensures
-    both are strings before concatenating them.
-
-    Parameters:
-    arg1 (int, float, str): The first value to be added.
-    arg2 (int, float, str): The second value to be added.
-
-    Returns:
-    int, float, str: The result of the addition or concatenation.
-    '''
-    # Convert 'arg1' and 'arg2' to their appropriate types
-    arg1conv = conv(arg1)
-    arg2conv = conv(arg2)
-    # If either 'arg1' or 'arg2' is a string, ensure they're both strings.
-    if isinstance(arg1conv, str) or isinstance(arg2conv, str):
-        arg1conv = str(arg1conv)
-        arg2conv = str(arg2conv)
-    return arg1conv + arg2conv
+def add2(*args):
+    sum = 0
+    for arg in args:
+        if isinstance(arg, (int, float)):
+            sum = sum + arg
+        elif isinstance(arg, (string)):
+            sum = sum + conv(arg)
+    return sum
