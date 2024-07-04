00e2c5942cc62d7ccdf4a8ca0a20723084430073
Leeo <91311423+lethmeshine@users.noreply.github.com>
2024-07-04 17:03:41 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,3 +1,4 @@
+
 '''
 The 'calc' library contains the 'add2' function that takes 2 values and adds
 them together. If either value is a string (or both of them are) 'add2' ensures
@@ -5,6 +6,7 @@ they are both strings, thereby resulting in a concatenated result.
 NOTE: If a value submitted to the 'add2' function is a float, it must be done so
 in quotes (i.e. as a string).
 '''
+
 
 def conv(value):
     '''
@@ -23,6 +25,7 @@ def conv(value):
             return float(value)
         except ValueError:
             return str(value)
+
 
 def add2(arg1, arg2):
     '''
@@ -45,6 +48,7 @@ def add2(arg1, arg2):
         arg1conv = str(arg1conv)
         arg2conv = str(arg2conv)
     return arg1conv + arg2conv
+
 
 def addAll(args):
     '''
